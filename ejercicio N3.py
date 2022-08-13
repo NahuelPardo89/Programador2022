18f5f9c1fbf641a9f9f94e88b6330b5adf6dc26d
soledadbrizuela <solebrizuela.17@gmail.com>
2022-08-13 13:25:04 -0300

    realice cambios bd

--- ejercicio N3.py
+++
@@ -1,5 +0,0 @@
-perro= [13,"Puppy", "12/12/2012" , "Macho", 123]
-#perro=(Id_Perro, nombre, fecha de nacimiento, sexo y dni del dueño)
-perro[2]="13/12/2012" #Fecha de nacimiento modificada
-perro[4]= 28957346 #DNI modificado
-print(perro)
