f82105fc447086e23e624b99eddd63e372737194
LautaroTorres <lautioptimus123@gmail.com>
2022-08-14 18:39:02 -0300

    Subiendo ej python

---
+++ LautaroPython4.py
@@ -0,0 +1,8 @@
+perro2=[14,"12/12/2012","Macho","23546987"]
+perro2.reverse()
+for cadena in perro2:
+   print (cadena)
+
+#Crear un lista en Python denominado “Perro2” que contenga los siguientes valores:
+#14,  Fido,  12/12/2012 , Macho, 23546987
+#Recorrer la lista “Perro” con un bucle y mostrar sus elementos por pantalla , comenzando desde el último hasta el primero.
