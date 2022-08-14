4202f8ead2659d294419c7a0912df40ebdf4a882
NahuelPardo89 <nahuel.pardo74@gmail.com>
2022-08-14 11:59:24 -0300

    Subiendo ejercicio 5 de python y Sql

---
+++ ejercicioNro5Python.py
@@ -0,0 +1,18 @@
+""" 
+Ejercicio 5:
+Crear una tupla en Python con el nombre de “Historial” la cual contenga los siguientes valores:
+
+          2350, 5960, 23000, 1000, 8900
+
+Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Puppy”. 
+Calcular el monto total gastado a lo largo del tiempo por atención de “Puppy”. 
+Si el gasto efectuado es menor a 30000, mostrar en pantalla dicho resultado, caso contrario mostrar el mensaje “Gastos por encima de lo presupuestado”.
+"""
+Historial =(2350, 5960, 23000, 1000, 8900)
+gasto=0
+for i in range(len(Historial)):
+    gasto+=Historial[i]
+if gasto<30000:
+    print("el gasto es: ",gasto)
+else:
+    print("Gastos por encima de lo presupuestado")
