7a0659bb917eea040f33308f465bfd4b031180fb
michaelFARIAS2022 <102944326+michaelFARIAS2022@users.noreply.github.com>
2022-08-20 00:06:43 -0300

    Update ejercicio 2 python.py

--- ejercicio 2 python.py
+++ ejercicio 2 python.py
@@ -1,4 +1,4 @@
 Dueno2 = ['23546987', 'Maria', 'Perez', '4789689', 'Pueyrredon_811']
-Dueno2 = ['Maria', '4789689', 'Pueyrredon_812']
-for i in reversed(Dueno2):
+Dueno2 = ['Maria', '4789689', 'Pueyrredon_811']
+for i in(Dueno2):
     print(i)
